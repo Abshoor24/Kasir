3b2ac0fad67db50dbd98d72f8fb228abbef0c3d4
Abshoor24 <muhammadnurulabshoor24@gmail.com>
2025-05-15 19:07:31 +0700

    FE-LP-2

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,9 +2,14 @@ import { useState, useEffect } from 'react'
 import './index.css';
 import axios from 'axios';
 import Navbar  from './component/navbar';
+import Store from './assets/store.png';
 
 function App() {
   const [message, setMessage] = useState('');
+
+  const goToRegister = () => {
+  window.location.href = 'http://localhost:8000/register'
+}
 
 useEffect(() => {
   axios.get('http://127.0.0.1:8000/api/greeting')
@@ -12,34 +17,44 @@ useEffect(() => {
     .catch(err => console.error(err));
 }, []);
 
-// const goToLogin = () => {
-//   window.location.href = 'http://localhost:8000/login'
-// }
-
-// const goToRegister = () => {
-//   window.location.href = 'http://localhost:8000/register'
-// }
-
   return (
     <>
     <Navbar />
 
-      <div className='m-auto ml-52 max-w-md mx-auto p-6'>
-      <h1 className='text-red-300'>Mengelola Toko</h1>
-      <h1 className='text-red-300'>Lebih Mudah</h1>
-      <div class="h-2 w-1/2 bg-gray-300 rounded-full my-4"></div>
-      <p className='text-xl'>Kelola stok, catat transaksi, dan pantau penjualan dari mana saja.
-      Cocok untuk Warung, UMKM, dan Bisnis Retail.
-      Tanpa ribet, langsung pakai!</p>
-      </div>
+  <div className="grid grid-cols-2 min-h-screen items-center px-52 bg-[#d2e7f6]">
+  {/* KIRI: Konten utama */}
+  <div className="space-y-6">
+    <h1 className="text-[#0F4C75] text-6xl">Mengelola Tokomu</h1>
+    <h1 className="text-[#0F4C75] font-bold text-6xl">Lebih Mudah</h1>
+    <div className="h-3 w-60 bg-gray-300 rounded-full"></div>
+    <p className="text-xl text-gray-500">
+      Kelola stok, catat transaksi, dan pantau penjualan dari mana saja. Cocok untuk Warung, UMKM, dan Bisnis Retail. Tanpa ribet, langsung pakai!
+    </p>
+<div className="flex items-center mt-5 space-x-2">
+  <div className="bg-gray-100 px-4 py-3 text-gray-500 rounded-xl border border-r-0 border-gray-300 max-w-md w-[300px] shadow-xl">
+    Create now
+  </div>
+  <button
+    className="bg-blue-500 text-white px-6 py-3 hover:bg-blue-600 transition-colors rounded-3xl border border-l-0 whitespace-nowrap shadow-xl"
+    onClick={goToRegister}
+  >
+    Create
+  </button>
+</div>
+  </div>
+
+<div className='flex justify-end'>
+<img 
+        src={Store} 
+        alt="Deskripsi Gambar" 
+       className='w-full max-w-md shadow-[0_25px_20px_-20px_rgba(0,0,0,0.3)]'
+      />
+</div>
 
 
+</div>
 
-    
     </>
-    
-    
-    
   );
 }
 
--- frontend-react/src/component/navbar.jsx
+++ frontend-react/src/component/navbar.jsx
@@ -11,30 +11,30 @@ const goToRegister = () => {
 
 
   return (
-     <nav className="w-full bg-[#BBE1FA] fixed top-0 left-0 z-50 mt-6 font-inter">
+     <nav className="w-full bg-[#d2e7f6] fixed top-0 left-0 z-50 mt-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
             <a href="/" className="text-4xl font-bold text-blue-600 tracking-tight">
-              LynkID Clone
+              Kasir
             </a>
           </div>
 
           {/* Menu */}
-          <div className="hidden md:flex items-center space-x-10 text-2xl mr-6">
+          <div className="hidden md:flex items-center space-x-10 text mr-6">
             <a href="#"
               onClick={(e) => {
                 e.preventDefault();
                 goToLogin();
               }}
-              className="text-black hover:text-blue-600 bold hover:scale-105 transition-transform duration-200"
+              className="text-black hover:text-blue-600 bold hover:scale-105 transition-transform duration-200  text-2xl font-inter"
             >
               Login
             </a>
             <button
               onClick={goToRegister}
-              className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 hover:scale-105 transition-transform duration-200"
+              className="bg-blue-600 text-white px-4 py-[4px] rounded-md hover:bg-blue-700 hover:scale-105 transition-transform duration-200 w-32 text-xl font-thin"
             >
               Register
             </button>
